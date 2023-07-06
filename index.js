c346b97c3972adecb2c6f190c019bd82778b1fd0
Aanchalsaroj123 <aanchal.saroj.ug21@nsut.ac.in>
2023-07-06 12:27:07 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express=require("express");
-const app=express();
-const dotenv=require("dotenv");
-const mongoose=require("mongoose");
-const authRoute=require("./routes/auth");
-const userRoute=require("./routes/users");
-const postRoute=require("./routes/posts");
-const categoryRoute=require("./routes/categories");
-const multer=require("multer");
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 const path = require("path");
 const port = process.env.PORT || 5000;
 
@@ -22,25 +22,31 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-  const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "images");
-    },
-    filename: (req, file, cb) => {
-      cb(null, req.body.name);
-    },
-  });
-  
-  const upload = multer({ storage: storage });
-  app.post("/api/upload", upload.single("file"), (req, res) => {
-    res.status(200).json("File has been uploaded");
-  });
-  
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-app.use("/api/auth",authRoute);
-app.use("/api/users",userRoute);
-app.use("/api/posts",postRoute);
-app.use("/api/categories",categoryRoute);
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+// Serve the frontend build files
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
